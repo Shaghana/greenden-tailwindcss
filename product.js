f1351072f70509d1d2efecaff62f4e1c0f4ce57c
Shaghana <shaghanan@gmail.com>
2023-11-29 03:42:37 +0400

    Added js

---
+++ product.js
@@ -0,0 +1,41 @@
+//Selecting sidenav and menu icon
+
+var sidenav=document.getElementById("sidenav")
+var menuicon=document.getElementById("menuicon")
+var closenav=document.getElementById("closenav")
+
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
+
+// Product search functionality
+var productContainer = document.getElementById("product-container")
+var search=document.getElementById("search")
+   
+    var productlist=productContainer.querySelectorAll("div")
+    //  console.log(productlist)
+
+    search.addEventListener("keyup",function(){
+        var enteredValue= event.target.value.toUpperCase()
+        // console.log(enteredValue)
+
+        for(count=0; count<productlist.length; count=count+1)
+        {
+            //h1 is inside the div
+            var productname = productlist[count].querySelector("h1").textContent
+            if(productname.toUpperCase().indexOf(enteredValue)<0)
+            {
+                productlist[count].style.display="none"
+            }
+            else{
+                productlist[count].style.display="block"
+            }
+        }
+    
+
+    })
